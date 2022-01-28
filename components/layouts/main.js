c6d6d5be494a0ef4d2d9dea522f53d88628da256
ValerianLukashyk <weonium@gmail.com>
2022-01-28 22:32:09 +0200

    add google site verification

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,17 +1,19 @@
 import Head from 'next/head'
 import Navbar from '../navbar'
 import NoSsr from '../no-ssr'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, useColorModeValue } from '@chakra-ui/react'
 import Planet from '../planet'
 import Footer from '../footer'
 import Contact from '../contact'
 
 const Main = ({ children, router, }) => {
-
+  const color = useColorModeValue('#f0e7db', '#202023')
   return (
     <Box as="main">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content={color} />
+        <meta name="google-site-verification" content="d-pB_seNFDrB08QpbqIAyJx9Oot3VCDQ9txyzd9xGPU" />
         <title>Valerian Lukashyk - Full-stack interactive developer</title>
       </Head>
 
